6e18804f59db12801a51bd3312a2094995ed2ed5
PhoeniX <n.shishmanov@gmail.com>
2017-04-19 14:27:42 +0300

    header, footer, layouts

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+
+        return $this->render('default/index.html.twig', array());
+    }
+}
--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,3 +1,16 @@
 /**
  * Created by phoenix on 4/19/17.
  */
+
+(function($) {
+
+    // DropDown UserMenu
+    $('#user_menu').hover(
+        function() {
+            $( '#user_menu ul' ).stop( true,true ).slideDown(200)
+        }, function() {
+            $( '#user_menu ul' ).stop( true,true ).slideUp(200)
+        }
+    );
+
+})(jQuery)
